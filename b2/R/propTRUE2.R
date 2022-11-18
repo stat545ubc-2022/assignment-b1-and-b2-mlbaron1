f05fc4d96519d5050bca25d313dc6af6ed2114d7
mlbaron1 <mbaronx3@gmail.com>
2022-11-17 21:32:55 -0800

    delete extra folder

---
+++ b2/R/propTRUE2.R
@@ -0,0 +1,18 @@
+
+#' Find the proportion of x=1
+#' The prop_true function which finds the proportion of the value x=1 over a list of variables
+#' @param x either a numerical vector or something coercible into one with TRUE/FALSE data, the default is TRUE is 1 and FALSE is 0.
+#' @returns Function will return the proportion of x = 1, in decimal form.
+#' @examples
+#' prop_true(x= c(FALSE,TRUE,TRUE)) #TRUE/FALSE data
+#' prop_true(x=c(1,0,1,0)) #binary numerical data
+#' prop_true(x= c(1,2,3)) #non-binary numerical data
+#' @export
+
+prop_true <- function(x) {
+  sum( ( x == 1 ) / length( x ) )
+}
+
+
+
+
